42a6dbaa8ea490177ca34120d661af97a08f5947
Obaid <Obaidurrahman031@gmail.com>
2023-01-30 23:16:09 +0530

    removed .env & updated port to production

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ dotenv.config();
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port =
+  process.env.NODE_ENV === "production" ? process.env.PORT || 80 : 5000;
 
 app.use(express.json());
 // app.use(bodyParser.json({ limit: "30mb", extended: true }));
