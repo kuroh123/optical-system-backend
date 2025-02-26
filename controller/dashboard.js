29c5de50ce6ecb77a4dd5510b030803df3c57065
Obaid <Obaidurrahman031@gmail.com>
2025-02-26 13:51:46 +0530

    FIX: Dashboard API

--- controller/dashboard.js
+++ controller/dashboard.js
@@ -4,55 +4,56 @@ import Patient from "../models/patient.model.js";
 // import PatientRequest from "../models/eyeDetail.model.js";
 
 function parseDate(dateStr, setEndOfDay = false) {
-  const [day, month, year] = dateStr.split('-').map(Number);
+  const [day, month, year] = dateStr.split("-").map(Number);
   if (setEndOfDay) {
-      return new Date(Date.UTC(year, month - 1, day, 23, 59, 59, 999));
+    return new Date(Date.UTC(year, month - 1, day, 23, 59, 59, 999));
   }
   return new Date(Date.UTC(year, month - 1, day));
 }
 
 export const dashboard = async (req, res) => {
-  const {from, to} = req.body
-  const fromDate = parseDate(from)
-  const toDate = parseDate(to, true)
-
-  console.log(fromDate, toDate)
+  // const { from, to } = req.body;
+  // const fromDate = parseDate(from);
+  // const toDate = parseDate(to, true);
+  const { location } = req.query;
 
   try {
     const patients = await Patient.find({
-      created_at: {
-        $gte: fromDate,
-        $lte: toDate
-      }
+      // created_at: {
+      //   $gte: fromDate,
+      //   $lte: toDate,
+      // },
     });
 
-    const billings = await Billing.find({
-      created_at: {
-        $gte: fromDate,
-        $lte: toDate
-      },
-      location: req.user.location
-    })
-  //   // const patientRequests = await PatientRequest.find({});
-  //   const bill = await Billing.find({});
-  //   let total_revenue = 0;
-  //   let total_vat = 0;
-  //   let total_balance = 0;
-  //   for (let i = 0; i < bill.length; i++) {
-  //     total_revenue += bill[i].paid_amount;
-  //     total_vat += bill[i].vat;
-  //     total_balance += bill[i].balance_amount;
-  //   }
+    // const billings = await Billing.find({
+    //   created_at: {
+    //     $gte: fromDate,
+    //     $lte: toDate,
+    //   },
+    // });
+    // const patientRequests = await PatientRequest.find({});
+    const bill = await Billing.find({ location: location })
+      .populate("transactions")
+      .exec();
+    let total_revenue = 0;
+    let total_vat = 0;
+    let total_balance = 0;
 
-  //   res.status(200).json({
-  //     patients: patients.length,
-  //     // patientRequests: patientRequests.length,
-  //     bill: bill.length,
-  //     total_revenue,
-  //     total_vat,
-  //     total_balance,
-  //   });
-  res.status(200).json(patients)
+    for (let i = 0; i < bill.length; i++) {
+      total_revenue += bill[i].paidAmount;
+      total_vat += bill[i].vat;
+      total_balance += parseInt(bill[i].balanceAmount);
+    }
+
+    res.status(200).json({
+      patients: patients.length,
+      // patientRequests: patientRequests.length,
+      bill: bill.length,
+      total_revenue: total_revenue.toFixed(3),
+      total_vat,
+      total_balance: total_balance.toFixed(3),
+    });
+    // res.status(200).json(patients);
   } catch (e) {
     res.status(404).json({ message: e.message });
   }
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -3,7 +3,8 @@ import express from "express";
 import { dashboard } from "../controller/dashboard.js";
 const router = express.Router();
 
-// router.route("/").get(users);
-router.post("/", authMiddleware, dashboard);
+router.get("/", authMiddleware, dashboard);
+
+// router.post("/", authMiddleware, dashboard);
 
 export default router;
